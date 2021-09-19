129f6344eb93dcbec76fe249446ceae470f04d96
fotino21 <ataturhan21@gmail.com>
2021-09-19 18:11:05 +0300

    adding an import

--- src/main/java/webApp/App.java
+++ src/main/java/webApp/App.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import spark.ModelAndView;
